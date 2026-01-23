4bbcfb62a155de49b68be79934a71608bd620738
Bence2107 <szabobence003@gmail.com>
2026-01-23 15:47:50 +0100

    Config to Angular work with API

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, ErrorHandler,} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {ApiConfiguration} from './api/api-configuration';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    {
+      provide: ErrorHandler,
+    },
+    {
+      provide: ApiConfiguration,
+      useFactory: () => {
+        const config = new ApiConfiguration();
+        config.rootUrl = "https://localhost:7221";
+        return config;
+      }
+    },
+  ]
 };
