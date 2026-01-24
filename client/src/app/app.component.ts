c43a7e5cb83d11651c11a830bbb07e1941563c2b
Bence2107 <szabobence003@gmail.com>
2026-01-24 23:00:26 +0100

    Begin header and sidenav

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatIcon} from '@angular/material/icon';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {NgIf} from '@angular/common';
+import {MatDivider} from '@angular/material/list';
+import {
+  MatAccordion,
+  MatExpansionPanel,
+  MatExpansionPanelHeader,
+  MatExpansionPanelTitle
+} from '@angular/material/expansion';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MatSidenavContainer, MatSidenav, RouterLink, RouterLinkActive, MatIcon, MatSidenavContent, NgIf, MatDivider, MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle, MatIconButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'drs-zona';
+  isScreenSmall = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe(result => {
+        this.isScreenSmall = result.matches;
+      });
+  }
 }
---
+++ client/src/app/components/header/header.component.ts
@@ -0,0 +1,64 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {NgIf} from '@angular/common';
+import {MatIcon} from '@angular/material/icon';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {MatTooltip} from '@angular/material/tooltip';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    NgIf,
+    MatIcon,
+    RouterLink,
+    RouterLinkActive,
+    MatButton,
+    MatTooltip,
+    MatSlideToggle
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  isDarkMode: boolean = true;
+  loggedId: any;
+  isLoggedIn: boolean = false;
+
+  @Output() toggleSidenav = new EventEmitter<void>();
+  isScreenSmall: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe(['(max-width: 990px)']).subscribe(result => {
+      this.isScreenSmall = result.matches;
+    });
+  }
+
+  ngOnInit(): void {
+    this.initializeTheme();
+  }
+
+  private initializeTheme(): void {
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    this.isDarkMode = savedTheme === 'dark';
+    this.setTheme(savedTheme);
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    const newTheme = this.isDarkMode ? 'dark' : 'light';
+    this.setTheme(newTheme);
+  }
+
+  setTheme(theme: string) {
+    if (theme === 'dark') {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
+    }
+    localStorage.setItem('theme', theme);
+  }
+}
