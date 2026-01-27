ecf74bbf702d7f286c99c4be7a27d15cd3b6fb94
Bence2107 <szabobence003@gmail.com>
2026-01-27 15:02:19 +0100

    Add response check Controller

---
+++ server/API/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Drs_Zona.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController(EfContext context) : ControllerBase
+{
+    [HttpGet("ping")]
+    public async Task<IActionResult> Ping()
+    {
+        var canConnect = await context.Database.CanConnectAsync();
+        return canConnect ? Ok(new { status = "Healthy" }) : StatusCode(500, "Database connection failed.");
+    }
+}
