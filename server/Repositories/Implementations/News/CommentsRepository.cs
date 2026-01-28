a63fd649a843daa050cea500d0ee15e56d0ebf2c
Bence2107 <szabobence003@gmail.com>
2026-01-28 22:28:47 +0100

    Fix error while get Comment

--- server/Repositories/Implementations/News/CommentsRepository.cs
+++ server/Repositories/Implementations/News/CommentsRepository.cs
@@ -56,10 +56,12 @@ public class CommentsRepository(EfContext context) : ICommentsRepository
         .ToList();
 
     public List<Comment> GetCommentsWithoutReplies(Guid articleId) => _comments
+        .Include(comment => comment.User)
         .Where(comment => comment.ArticleId == articleId && comment.ReplyToCommentId == null)
         .ToList();
 
     public List<Comment> GetRepliesToAComment(Guid replyCommentId) => _comments
+        .Include(comment => comment.User)
         .Where(comment => comment.ReplyToCommentId == replyCommentId)
         .ToList();
     public bool CheckIfIdExists(Guid id) => _comments.Any(comment => comment.Id == id);
