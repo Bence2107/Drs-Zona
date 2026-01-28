c1f4c7aec89ca8b59ff25fb356be9372f1fe0c67
Bence2107 <szabobence003@gmail.com>
2026-01-28 20:04:38 +0100

    Add Article page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {NewsComponent} from './pages/articles/news/news.component';
 import {ReviewsComponent} from './pages/articles/reviews/reviews.component';
+import {ArticleComponent} from './pages/article/article.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: "home", component: HomeComponent },
   { path: "news", component: NewsComponent },
   { path: "reviews", component: ReviewsComponent },
+  { path: "article/:slug", component: ArticleComponent },
 ];
---
+++ client/src/app/pages/article/article.component.ts
@@ -0,0 +1,62 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {MatCard, MatCardContent, MatCardFooter} from '@angular/material/card';
+import {MatIcon} from '@angular/material/icon';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {NgForOf, NgIf} from '@angular/common';
+import {MatButton} from '@angular/material/button';
+import {MatProgressBar} from '@angular/material/progress-bar';
+import {ErrorDisplayComponent} from '../../components/error-display/error-display.component';
+import {ArticleDetailDto} from '../../api/models/article-detail-dto';
+import {ArticleService} from '../../services/article.service';
+
+@Component({
+  selector: 'app-article',
+  imports: [
+    MatCard,
+    RouterLink,
+    NgIf,
+    MatButton,
+    MatCardContent,
+    MatCardFooter,
+    ErrorDisplayComponent,
+    NgForOf
+  ],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.scss'
+})
+export class ArticleComponent implements OnInit {
+  @Input() article: ArticleDetailDto | any;
+  isLoading = false;
+  errorOccurred = false;
+
+  constructor(private articleService: ArticleService, private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.fetchArticle();
+  }
+
+  fetchArticle() {
+    const slug = this.route.snapshot.paramMap.get('slug');
+
+    if(slug === null) {
+       this.router.navigate(['/news']);
+    }
+
+    this.isLoading = true;
+    this.errorOccurred = false;
+
+    if (slug != null) {
+      this.articleService.getBySlug(slug).subscribe({
+        next: (data) => {
+          this.article = data;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error(err);
+          this.isLoading = false;
+          this.errorOccurred = true;
+        }
+      });
+    }
+  }
+}
