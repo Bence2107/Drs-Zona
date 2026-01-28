b132db18dd4ddad9b719c5fd877a811872d897f5
Bence2107 <szabobence003@gmail.com>
2026-01-28 12:04:23 +0100

    Split Articles and summarys in frontend

--- client/src/app/services/article.service.ts
+++ client/src/app/services/article.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ApiConfiguration} from '../api/api-configuration';
 import {Observable} from 'rxjs';
-import {apiArticleGetAllGet$Json, apiArticleGetRecentNumberGet$Json} from '../api/functions';
+import {
+  apiArticleGetAllArticlesGet$Json,
+  apiArticleGetAllSummaryGet$Json,
+  apiArticleGetRecentNumberGet$Json
+} from '../api/functions';
 import {map} from 'rxjs/operators';
 import {ArticleListDto} from '../api/models/article-list-dto';
 
@@ -13,8 +17,18 @@ export class ArticleService {
 
   constructor(private http: HttpClient, private apiConfig: ApiConfiguration) { }
 
-  getAll() : Observable<ArticleListDto[]> {
-    return apiArticleGetAllGet$Json(this.http, this.apiConfig.rootUrl).pipe(
+  getAllArticles() : Observable<ArticleListDto[]> {
+    return apiArticleGetAllArticlesGet$Json(this.http, this.apiConfig.rootUrl).pipe(
+      map(response => {
+        const body = response.body as any;
+        return body.value as ArticleListDto[] ??[]
+      })
+    )
+  }
+
+
+  getAllSummary() : Observable<ArticleListDto[]> {
+    return apiArticleGetAllSummaryGet$Json(this.http, this.apiConfig.rootUrl).pipe(
       map(response => {
         const body = response.body as any;
         return body.value as ArticleListDto[] ??[]
