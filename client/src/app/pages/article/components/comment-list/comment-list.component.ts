565f03853f3d57076552d2c0fed95a24147e80fa
Bence2107 <szabobence003@gmail.com>
2026-01-29 21:23:09 +0100

    Delete unnecessary imports

--- client/src/app/pages/article/components/comment-list/comment-list.component.ts
+++ client/src/app/pages/article/components/comment-list/comment-list.component.ts
@@ -8,28 +8,14 @@ import {MatDivider} from '@angular/material/list';
 import {CommentService} from '../../../../services/comment.service';
 import {UIComment} from '../../../../api/models/ui-comment';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatIconButton} from '@angular/material/button';
 import {CommentItemComponent} from './comment-item/comment-item.component';
 
 @Component({
   selector: 'app-comment-list',
   imports: [
     MatCard,
-    MatProgressBar,
-    NgIf,
-    MatCardHeader,
-    MatCardTitle,
-    MatCardSubtitle,
-    MatCardContent,
-    MatExpansionPanel,
     NgForOf,
-    MatExpansionPanelHeader,
-    MatExpansionPanelTitle,
-    MatIcon,
-    MatDivider,
     ReactiveFormsModule,
-    DatePipe,
-    MatIconButton,
     CommentItemComponent,
   ],
   templateUrl: './comment-list.component.html',
