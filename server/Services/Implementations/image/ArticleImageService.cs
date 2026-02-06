9a8022e724e1ac2c8cfc6aa862413bb769349d07
Bence2107 <szabobence003@gmail.com>
2026-02-06 11:55:49 +0100

    Write Article Image Service

---
+++ server/Services/Implementations/image/ArticleImageService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Services.Interfaces.images;
+
+namespace Services.Implementations.image;
+
+public class ArticleImageService(IWebHostEnvironment env): IArticleImageService
+{
+    public string GetImageUrl(string slug, string imageName)
+    {
+        return $"/uploads/images/articles/{slug}/{imageName}";
+    }
+    
+    public async Task<string> SaveImage(string slug, IFormFile file, string imageName)
+    {
+        var uploadsPath = Path.Combine(env.ContentRootPath, "uploads", "images", "articles", slug);
+        Directory.CreateDirectory(uploadsPath);
+        
+        var fileName = $"{imageName}{Path.GetExtension(file.FileName)}";
+        var filePath = Path.Combine(uploadsPath, fileName);
+
+        await using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+        
+        return GetImageUrl(slug, imageName);
+    }
+    
+    public void DeleteArticleImages(string slug)
+    {
+        var folderPath = Path.Combine(env.ContentRootPath, "uploads", "images", "articles", slug);
+        if (Directory.Exists(folderPath))
+        {
+            Directory.Delete(folderPath, true);
+        }
+    }
+}
---
+++ server/Services/Interfaces/images/IArticleImageService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Services.Interfaces.images;
+
+public interface IArticleImageService
+{
+    string GetImageUrl(string slug, string imageName);
+    Task<string> SaveImage(string slug, IFormFile file, string imageName);
+    void DeleteArticleImages(string slug);
+
+}
